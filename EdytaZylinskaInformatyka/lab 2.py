ad77fa5afbffe19f933e8014c0886822e3a9b5de
ze39099 <82755164+ze39099@users.noreply.github.com>
2021-05-09 12:44:32 +0200

    Add files via upload

---
+++ EdytaZylinskaInformatyka/lab 2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  9 12:42:54 2021
+
+@author: Edyta
+"""
+n = 5
+silnia = 1
+i = 1
+print ('silnia = ', silnia)
+print ('i = ', i)
+while i <= n:
+    silnia = silnia * i 
+    print ('silnia = ', silnia)
+    i = i + 1
+    print ('i = ', i)
+print ('Koniec  silnia = ', silnia)
+    
